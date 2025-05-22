5ac554a3a541f836de0d697ba158cb10bf9d37d1
Thang <minhthang7209@gmail.com>
2025-05-22 11:32:52 +0300

    Create constructors

--- IntroToOPP/IntroToOPP.cpp
+++ IntroToOPP/IntroToOPP.cpp
@@ -8,6 +8,11 @@ private:
     int year;
     string type;
 public:
+    Building(int y, string t){
+        set_data(y, t);
+    }
+
+
     void set_data(int y, string t) {
         year = y;
         type = t;
@@ -22,16 +27,15 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     //Класи
-    Building School;
+    Building School(2001,"School");
     /*School.type = "School";
     School.year = 2001;*/
-    School.set_data(2001, "School");
+    //School.set_data(2001, "School");
     School.get_info();
 
-    Building house;
+    Building house(2021,"House");
    /* house.type = "House";
     house.year = 2021;*/
-    house.set_data(2021, "House");
+    /*house.set_data(2021, "House");*/
     house.get_info();
-
 }
